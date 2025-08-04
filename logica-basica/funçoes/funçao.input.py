037303ab44e53d8d2c5ac12c7806e09d05f619c9
JvCoding5 <joao.victormg789@gmail.com>
2025-08-04 13:17:42 -0300

    Atulização

---
+++ logica-basica/funçoes/funçao.input.py
@@ -0,0 +1,20 @@
+# --- Função Input ---
+
+"""
+A função `input` é usada para receber entrada do usuário em Python.
+Ela permite que o programa pause e aguarde que o usuário digite algo no teclado.
+O valor digitado pelo usuário é retornado como uma string.
+"""
+
+input('Qual o seu nome? ') # Exibe a mensagem e aguarda a entrada do usuário
+
+nome = input('Qual o seu nome? ')  # A entrada do usuário é armazenada na variável 'nome'
+# O valor digitado pelo usuário será uma string, mesmo que seja um número, por padrão.
+
+print(f'Olá, {nome=}!')  # Exibe uma mensagem personalizada usando o nome fornecido pelo usuário
+# O sinal de = após a variável 'nome' é uma forma de formatar a string para mostrar o nome da variável
+# e seu valor.
+
+
+
+
