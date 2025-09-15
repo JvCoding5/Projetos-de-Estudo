8981d95fb9431db11f522f8a4daf85f56b9329e2
João Victor da SIlva <joao.victormg789@gmail.com>
2025-09-15 20:09:41 -0300

    Atualização

---
+++ logica-basica/variaveis/metodos-list-append-pop.py
@@ -0,0 +1,32 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append, insert, pop, del, clear, extend, +
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+#        0   1   2   3   4   5
+lista = [10, 20, 30, 40]
+# lista[2] = 300
+# del lista[2]
+# print(lista)
+# print(lista[2])
+lista.append(50)
+lista.pop()
+lista.append(60)
+lista.append(70)
+ultimo_valor = lista.pop(3)
+print(lista, 'Removido,', ultimo_valor)
+
+
+
+
+animes_assistidos = ["Boku no Hero", "One piece", "Solo leveling", "VinlandSaga", "Dragon Ball", "Hajime no Hippo" \
+            "Baki", "Shuumatsu no Valkyre", "Demon Slayer", "Frieren", "Blue Lock", "Black Clover", "Tower of god", "Kaiju no 8"]
+
+animes_assistidos.pop()
+animes_assistidos.append("Viral Hit")
+print (animes_assistidos)
